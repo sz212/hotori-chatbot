dab8a22c5d4483f038bff16c3d53954331ba0825
sz212 <mike.zheng@live.co.uk>
2026-03-01 22:23:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Hotori chatbot running on port ${PORT}`);
 });
